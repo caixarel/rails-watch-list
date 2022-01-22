d0e2f89e67b1429aeffbea87426c1dd1723715fc
Bruno Godinho <godinho_bruno@hotmail.com>
2022-01-22 16:46:40 +0100

    done , no style yet

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+        redirect_to list_path(List.find(params[:list_id]))
+    end
+  end
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(List.find(params[:list_id]))
+
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
